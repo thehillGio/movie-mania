fc16ba6f2318d8603251d4f6dd43084c51cf5d13
thehillGio <156510985+thehillGio@users.noreply.github.com>
2024-01-19 15:48:46 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,109 @@
+
+let index =0;
+
+const totalWorkItems =$('.work-item').length;
+
+$(window).on("load", function(){
+    $(".preloader").addClass("loaded");
+})
+
+
+$(document).ready(function(){
+
+    $('.nav-toggle').click(function(){
+        $('.header .nav').slideToggle();
+    })
+
+    $('.header .nav a').click(function(){
+        if($(window).width()< 768){
+            $('.header .nav').slideToggle();
+        }
+
+    })
+
+    //fixed header
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $('.header').addClass("fixed");
+        }else{
+            $('.header').removeClass("fixed");
+        }
+    })
+
+
+    $('a').on('click',function(){
+        if(this.hash !== ""){
+            event.preventDefault();
+
+            var hash = this.hash;
+            $('html,body').animate({
+                scrollTop: $(hash).offset().top
+            },800,function(){
+                window.location.hash = hash;
+            })
+        }
+    })
+
+    //Lightbox
+    const wHeight =$(window).height();
+    $(".lightbox-img").css("max-height",wHeight+"px");
+
+    $(".work-item-inner").click(function(){
+        index = $(this).parent(".work-item").index();
+        $(".lightbox").addClass("open");
+        lightboxSlideShow();
+    })
+
+    $(".lightbox .prev").click(function(){
+        if(index == 0){
+            index = totalWorkItems - 1
+        }else{
+            index--;
+        }
+        lightboxSlideShow();
+    })
+
+    $('.lightbox .next').click(function(){
+        if(index == totalWorkItems - 1){
+            index = 0
+        }
+        else{
+            index++;
+        }
+        lightboxSlideShow();
+    })
+
+    $(".lightbox-close").click(function(){
+        $(".lightbox").removeClass("open");
+    })
+
+    $(".lightbox").click(function(event){
+        if($(event.target).hasClass("lightbox")){
+            $(this).removeClass("open");
+        }
+    })
+
+
+    //function
+
+    function lightboxSlideShow(){
+        const imgSrc = $(".work-item").eq(index).find("img").attr("data-large");
+        const category = $(".work-item").eq(index).find("h4").html();
+        $(".lightbox-img").attr("src",imgSrc);
+        $(".lightbox-category").html(category);
+        $(".lightbox-counter").html(totalWorkItems+"/"+(index+1));
+    }
+
+
+    $("#button").click(function(){
+        $("#dialog").dialog("open");
+    })
+
+    $("#dialog").dialog({
+        title: "Thank You For Contacting Us",
+        closeOnEscape: false,
+        modal: true,
+        autoOpen:false,
+        draggable:false
+    })
+})
